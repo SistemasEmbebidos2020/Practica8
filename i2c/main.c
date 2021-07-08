88b83014ec56cce2d8ed9cc52a68909c9d442500
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2021-07-07 22:31:16 -0500

    Create main.c

---
+++ i2c/main.c
@@ -0,0 +1,17 @@
+#include "funciones.h"
+
+int main()
+ { 
+      serial_begin();
+      i2c_init();
+      lcd_init();
+     lcd_msg("Temperatura es: ");
+
+     while (1)
+      {
+     lcd_cursor(1,7);
+	 lcd_sent();    
+
+      }
+      return 0;
+ }
