3720314400ac9521a39ee36b52f95e7b9f64776c
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-10 15:05:49 -0500

    Create i2c.c

---
+++ fisico/i2c.c
@@ -0,0 +1,14 @@
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd1(0x27,16,2);
+
+
+void setup() {
+  lcd1.init();
+  lcd1.backlight();
+  lcd1.setCursor(1,0);
+  lcd1.print("hola mundo");
+}
+
+void loop() {
+  _delay_ms(1500);
+}
