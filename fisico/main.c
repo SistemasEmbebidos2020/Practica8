5fe1c5757a925b69e90463e9cab71ec076864767
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-11 14:11:55 -0500

    Update main.c

--- fisico/main.c
+++ fisico/main.c
@@ -1,7 +1,7 @@
 #include "uart.h"
 
 
-char* mensaje = "";
+const char* mensaje ="";
 
 int main()
  { 
@@ -10,11 +10,12 @@ serial_begin();
 while (1)
 {
 serial_println_str("Bienvenido a Sistemas Embebidos");
-serial_println_str("ingresa tu nombre seguido de enter");
+serial_println_str("Ingresa tu nombre seguido de enter");
 while (!is_data_ready()){_delay_ms(20);}
 mensaje = get_RX_buffer();
+serial_print_str("Bienvenido ");
 serial_println_str(mensaje);
+_delay_ms(250);
 }
- _delay_ms(500);
    return 0;
 }
