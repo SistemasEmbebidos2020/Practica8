b5665bd0aba5cf9efb0b49c19108c972c0a24a82
SistemasEmbebidos <78398897+SistemasEmbebidos2020@users.noreply.github.com>
2022-07-11 14:11:29 -0500

    Update main.c

--- fisico/main.c
+++ fisico/main.c
@@ -10,9 +10,11 @@ serial_begin();
 while (1)
 {
 serial_println_str("Bienvenido a Sistemas Embebidos");
-while (!is_data_ready()){_delay_ms(2);}
+serial_println_str("ingresa tu nombre seguido de enter");
+while (!is_data_ready()){_delay_ms(20);}
 mensaje = get_RX_buffer();
 serial_println_str(mensaje);
 }
+ _delay_ms(500);
    return 0;
 }
